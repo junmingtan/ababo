22f383567c8a8e511d6bf0d731e9b242bcf48685
Tan Jun Ming <junming1996@hotmail.com>
2018-11-09 02:52:45 +0800

    updated profile and donation page

--- app/src/main/java/com/ababo/tanjunming/ababo/EditProfile.java
+++ app/src/main/java/com/ababo/tanjunming/ababo/EditProfile.java
@@ -1,18 +1,15 @@
 package com.ababo.tanjunming.ababo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class EditProfile extends AppCompatActivity {
 
+    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,41 +18,27 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    startActivity(new Intent(MainActivity.this, Donate.class));
+                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
         }
     };
 
-    private ProgressBar mNextDonation;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit_profile);
 
+        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setSelectedItemId(R.id.navigation_home);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        ImageButton mEditProfileButton = (ImageButton) findViewById(R.id.editProfile);
-        mEditProfileButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Donate.class));;
-            }
-        });
-
-        mNextDonation = (ProgressBar) findViewById(R.id.progressBar);
-        mNextDonation.setProgress(70);
-
     }
 
 }
